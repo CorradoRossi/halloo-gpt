4f32e8d8a7f7bbc00cefa79b8e79d8688476989b
Rossi Meacham <rossi@frugaltricks.com>
2023-04-27 06:14:41 -0400

    send it

--- pages/index.tsx
+++ pages/index.tsx
@@ -108,7 +108,7 @@ const Home: NextPage = () => {
 
       <Header />
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-12 sm:mt-6">
-        <a
+        {/*<a
           className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-4 py-2 text-sm text-gray-600 shadow-md transition-colors hover:bg-gray-100 mb-5"
           href="https://github.com/CorradoRossi/halloo-gpt"
           target="_blank"
@@ -116,10 +116,10 @@ const Home: NextPage = () => {
         >
           <Github />
           <p>View on GitHub</p>
-        </a>
+        </a>*/}
         <h1 className="sm:text-4xl text-2xl max-w-[708px] font-bold text-slate-900">
-          Want to make an impression? Enter a Twitter handle and get a
-          distinctive tweetable greeting via AI wizardry.
+          Want to make an impression? Enter a Twitter handle and a vibe for a
+          GPT grade introduction
         </h1>
         <div className="max-w-xl w-full">
           <div className="flex mt-10 items-center space-x-3">
