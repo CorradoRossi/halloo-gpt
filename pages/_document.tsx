79fe1021d1fc9294c169412bf711366f4b23a361
Rossi Meacham <rossi@frugaltricks.com>
2023-04-26 22:41:37 -0400

    halloo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,35 +8,34 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <meta
             name="description"
-            content="Generate your next whisper in seconds."
+            content="Generate your next halloo in seconds."
           />
-          <meta name="og:title" content="WhisperGPT" />
+          <meta name="og:title" content="HallooGPT" />
           <meta
             name="og:description"
-            content="Generate your next whisper in seconds."
+            content="Generate your next halloo in seconds."
           />
           <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:site" content="@vulture990" />
           <meta name="twitter:creator" content="@vulture990" />
-          <meta name="twitter:title" content="WhisperGPT" />
+          <meta name="twitter:title" content="HallooGPT" />
           <meta
             name="twitter:description"
-            content="Generate your next whisper in seconds."
+            content="Generate your next halloo in seconds."
           />
           <meta
             name="twitter:image"
-            content="https://whisper-gpt.vercel.app/og.png"
+            content="https://halloo-gpt.vercel.app/og.png"
           />
           <meta
             name="og:image"
-            content="https://whisper-gpt.vercel.app/og.png"
+            content="https://halloo-gpt.vercel.app/og.png"
           />
           <meta name="og:image:width" content="1200" />
           <meta name="og:image:height" content="630" />
-          <meta name="og:url" content="https://whisper-gpt.vercel.app/" />
-          <meta name="og:site_name" content="WhisperGPT" />
+          <meta name="og:url" content="https://halloo-gpt.vercel.app/" />
+          <meta name="og:site_name" content="HallooGPT" />
           <meta name="og:type" content="website" />
-
         </Head>
         <body>
           <Main />
