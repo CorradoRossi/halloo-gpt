441ab684773ca8cb56867998c82fe0aa4a71ab95
Rossi Meacham <rossi@frugaltricks.com>
2023-04-27 05:23:55 -0400

    send it

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center w-full mt-5 border-b-2 pb-7 sm:px-4 px-2">
+    <header className="flex justify-between items-center w-full mt-4 border-b-2 pb-4 sm:px-4 px-2">
       <Link href="/" className="flex space-x-3">
         <h1 className="sm:text-4xl text-2xl font-bold ml-2 tracking-tight">
           Halloo
